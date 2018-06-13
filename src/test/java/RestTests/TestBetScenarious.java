a09a2de99c22cf46ff04274a27f6cbeed81c339a
coffe <coffeacc10@gmail.com>
2018-06-14 01:17:33 +0300

    draft new tests

--- src/test/java/RestTests/TestBetScenarious.java
+++ src/test/java/RestTests/TestBetScenarious.java
@@ -22,6 +22,7 @@ public class TestBetScenarious {
     String token2 = "";
     String token3 = "";
     int idRandomBet;
+    int groupBetId;
 
     @Story("Random Flow checks")
     @Test
@@ -32,5 +33,37 @@ public void checkbetRandomCreationJoin(){
    // userCon.checkConfirmLineUp(token2);
     idRandomBet= betCon.createBetRandom(token1);
     betCon.joinRandomBet(token2,idRandomBet);
+}
+
+
+@Story("Group Bets")
+//Draft
+    @Test
+    public void checkGrouoBet(){
+        token1 = userCon.signIn(email1, password1);
+        groupBetId = betCon.createBetRandom(token1);
+
+    for (int i=1;i<10;i++){
+        String token= userCon.signIn("coffe9"+i+"@mailinator.com","Qwe1156q@@");
+        userCon.checkConfirmLineUp(token);
+        betCon.joinRandomBet(token,groupBetId);
+
+    }
+}
+
+
+@Story("Mass bets")
+    @Test
+//draft
+    public void createMassBets(){
+
+        token1 = userCon.signIn(email1, password1);
+        token2 = userCon.signIn(email2, password1);
+        userCon.checkConfirmLineUp(token1);
+        userCon.checkConfirmLineUp(token2);
+        for(int i=0; i<100;i++){
+           idRandomBet= betCon.createBetRandom(token1);
+            betCon.joinRandomBet(token2,idRandomBet);
+        }
 }
 }
